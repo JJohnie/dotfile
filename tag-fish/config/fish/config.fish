9850b23103125f5b78fa88b727dcc70d49a7941a
jjohnie <johniehe023@gmail.com>
2023-09-23 14:01:01 +0800

    update

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -6,10 +6,11 @@ end
 alias g="git"
 alias gcl="git clone"
 alias gst="git status"
-alias gpu="git pull"
+alias gpul="git pull"
 alias gcmt="git commit"
 alias gd="git add"
 alias gini="git init"
+alias gpsh="git push"
 
 alias nf="neofetch"
 
